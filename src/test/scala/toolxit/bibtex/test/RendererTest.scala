c6b99ce813ab3b6a4c039809fb7fb1ffcff008fa
Lucas Satabin <lucassat@n7mm.org>
2012-04-10 10:33:23 +0200

    test added

---
+++ src/test/scala/toolxit/bibtex/test/RendererTest.scala
@@ -0,0 +1,46 @@
+package toolxit.bibtex
+package test
+
+import renderer._
+import java.io.{ FileInputStream, InputStreamReader, FileOutputStream, OutputStreamWriter, File }
+import scala.util.Properties
+
+object Renderer extends App {
+
+  import BibTeXParsers._
+
+  val input = if (args.length == 0)
+    "doc/metrics.bib"
+  else
+    args(0)
+
+  val encoding = if (args.length < 2)
+    "ISO-8859-15"
+  else
+    args(1)
+
+  parseAll(bibFile,
+    new InputStreamReader(new FileInputStream(input), encoding)) match {
+      case Success(res, _) =>
+        val renderer = new HtmlRenderer(res, Map.empty).groupByField("year", Descending)
+        val html =
+          <html>
+            <head>
+              <style type="text/css">{ renderer.defaultCSS }</style>
+            </head>
+            <body>
+              { renderer.render }
+            </body>
+          </html>
+
+        val file = new File(Properties.userHome, "bib.html")
+        file.createNewFile
+        val writer = new OutputStreamWriter(
+          new FileOutputStream(file), encoding)
+        writer.write(html.toString)
+        writer.flush
+        writer.close
+      case fail => println(fail)
+    }
+
+}
